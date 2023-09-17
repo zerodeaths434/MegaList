0b52e1d2870da9b79ae5027d136153482c47ba89
Akash Panday <akashpanday11@gmail.com>
2023-09-17 11:56:56 +0530

    Added images locally

--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -1,6 +1,10 @@
 import "./home.css";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import animepic from "./anime.jpg";
+import gamepic from "./games.jpg";
+import moviespic from "./movies.jpg";
+import seriespic from "./series.jpg";
 
 function Home() {
   const user = useSelector((state) => state.user);
@@ -9,38 +13,26 @@ function Home() {
       <div className="mainDiv">
         <Link to={`${user || "guest"}?list=anime`}>
           <div className="contentDiv animeDiv">
-            <img
-              src="https://wallpapers.com/images/hd/anime-collage-1920-x-1080-wallpaper-fwx8xyvh2rd4ju8n.jpg"
-              alt="Anime"
-            />
-            <h1 className="trying">ANIME</h1>
+            <img src={animepic} alt="Anime background" />
+            <h1 className="home-card-title">ANIME</h1>
           </div>
         </Link>
         <Link to={`${user || "guest"}?list=movie`}>
           <div className="contentDiv movieDiv">
-            <img
-              src="https://e0.pxfuel.com/wallpapers/442/396/desktop-wallpaper-film-posters-collage-movies-resolution.jpg"
-              alt="Anime"
-            />
-            <h1 className="trying">MOVIES</h1>
+            <img src={moviespic} alt="Movies background" />
+            <h1 className="home-card-title">MOVIES</h1>
           </div>
         </Link>
         <Link to={`${user || "guest"}?list=series`}>
           <div className="contentDiv seriesDiv">
-            <img
-              src="https://i.pinimg.com/originals/ba/05/5e/ba055e048c003933747a91c64c20cc17.jpg"
-              alt="Anime"
-            />
-            <h1 className="trying">SERIES</h1>
+            <img src={seriespic} alt="Series background" />
+            <h1 className="home-card-title">SERIES</h1>
           </div>
         </Link>
         <Link to={`${user || "guest"}?list=game`}>
           <div className="contentDiv booksDiv">
-            <img
-              src="https://c4.wallpaperflare.com/wallpaper/96/92/869/game-games-2014-best-wallpaper-preview.jpg"
-              alt="Anime"
-            />
-            <h1 className="trying">GAMES</h1>
+            <img src={gamepic} alt="Games background" />
+            <h1 className="home-card-title">GAMES</h1>
           </div>
         </Link>
       </div>
